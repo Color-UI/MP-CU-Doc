aa771aa5410f29aa440483ad1202ec124896f4d2
ZaiZai <806606688@qq.com>
2021-12-17 18:01:25 +0800

    up

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,52 @@
+module.exports = {
+    lang: 'zh-CN',
+    title: 'MP-CU-Doc',
+    description: 'MP-CU文档，ColorUI 微信原生小程序UI组件库',
+    head: [
+        ['link', {rel: 'icon', href: '/images/icon/favicon.ico'}]
+    ],
+    themeConfig: {
+        logo: '/images/icon/logo.png',
+        navbar: [
+            {
+                text: '组件',
+                link: '/guide/'
+            },
+            {
+                text: 'Api',
+                link: '/api/'
+            },
+            {
+                text: '实验室',
+                link: '/test/'
+            },
+            {
+                text: '模板',
+                link: '/tpl/'
+            },
+            {
+                text: '定制',
+                link: '/custom/'
+            },
+            {
+                text: '生态',
+                children: [
+                    {text:'uni-app版', link: 'https://github.com/weilanwl/coloruiBeta'},
+                    {text:'2.x版本', link: 'https://github.com/weilanwl/ColorUI'}
+                ]
+            },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/Color-UI/MP-CU'
+            },
+        ]
+    },
+    plugins: [
+        [
+            '@vuepress/plugin-search', {
+                locales: {'/': {placeholder: 'Search'}, '/zh/': {placeholder: '搜索'}},
+                isSearchable: (page) => page.path !== '/', // 排除首页
+            },
+        ],
+    ],
+}
---
+++ docs/README.md
@@ -0,0 +1,19 @@
+---
+home: true
+heroImage: /images/icon/logo.png
+actions:
+- text: 开始使用
+  link: /guide/
+  type: primary
+- text: GitHub
+  link: https://github.com/Color-UI/MP-CU
+  type: secondary
+features:
+- title: 组件
+  details: ColorUI3.x 对比 ColorUI2.x，从之前的class方式，转为了组件化的方式，并提供了丰富的自定义参数以及插槽
+- title: 颜色
+  details: 漂亮及丰富的色彩，一直是ColorUI的亮点，ColorUI3.x内置了比ColorUI2.x更多的预设颜色（普通色、渐变色等）
+- title: 主题
+  details: 内置了两种主题，白色和深色，在开发使用时，只需要简单的调用，即可在微信原生小程序上拥有全局主题功能
+footer: Copyright © weilanwl.com Design. colorui-team Development.  All Rights Reservecd
+---
